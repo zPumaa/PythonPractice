fbc64b96a8a0672804a9658033ef3c48b63b73ce
Usman <usmanali6184@gmail.com>
2024-08-13 23:14:52 +0100

    FlaskWTF work

---
+++ Day61FlaskWTF/main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+from flask_bootstrap import Bootstrap5
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+
+app = Flask(__name__)
+app.secret_key = "abc"
+bootstrap = Bootstrap5(app)
+
+class MyForm(FlaskForm):
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    email = StringField(label='Email', validators=[DataRequired(), Email(check_deliverability=True)])
+    submit = SubmitField(label='Log In')
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = MyForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
